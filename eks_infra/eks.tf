2be00d7e679a986d8d4ee57a8ea955508e5bdfae
ngthecoder <150304266+ngthecoder@users.noreply.github.com>
2026-03-22 21:45:39 -0400

    feat: Add AWS EKS cluster and node group configuration

---
+++ eks_infra/eks.tf
@@ -0,0 +1,43 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name = "${var.project_name}-eks-cluster"
+
+  access_config {
+    authentication_mode = "API"
+  }
+
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.31"
+
+  vpc_config {
+    subnet_ids = aws_subnet.public_subnet[*].id
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_role_attachment,
+  ]
+}
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "${var.project_name}-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = aws_subnet.private_subnet[*].id
+
+  instance_types = ["t3.medium"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ecr_policy
+  ]
+}
