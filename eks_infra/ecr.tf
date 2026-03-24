784e7c6b81f00a2f38e56da33943aa6a41e26c90
ngthecoder <150304266+ngthecoder@users.noreply.github.com>
2026-03-23 23:04:50 -0400

    infra: Enable force deletion for ECR repositories and add detailed teardown instructions to the README.

--- eks_infra/ecr.tf
+++ eks_infra/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "ecr_repo" {
   name                 = "${var.project_name}-ecr-repo"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
   image_scanning_configuration {
     scan_on_push = true
   }
