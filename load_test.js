ceb5baa9909ce278c1f3c98b2e4e46e81e122849
ngthecoder <150304266+ngthecoder@users.noreply.github.com>
2026-03-27 21:33:34 -0400

    feat: Add Horizontal Pod Autoscaler, update load test to a spike scenario with a new endpoint, and increase EKS node group desired and max sizes.

--- load_test.js
+++ load_test.js
@@ -2,15 +2,19 @@ import http from 'k6/http';
 
 export const options = {
     scenarios: {
-        contacts: {
-            executor: 'constant-vus',
-            vus: 10,
-            duration: '30s',
+        spike: {
+            executor: 'ramping-vus',
+            stages: [
+                { duration: '10s', target: 1 },
+                { duration: '5s', target: 40 },
+                { duration: '30s', target: 40 },
+                { duration: '10s', target: 1 },
+            ],
         },
     },
 };
 
-const url = 'http://a0e07c34464164758ac9f1c05784300b-1532965653.us-east-1.elb.amazonaws.com:8080/analyze';
+const url = 'http://af6076ec46df3438b9474c35a3f52f62-1584184945.us-east-1.elb.amazonaws.com:8080/analyze';
 
 export default function () {
     let data = { text: 'I lost my wallet.' };
